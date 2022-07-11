1d9b199b3cfacbb14606565b95c7c206f67dded6
Nuzri <92069098+Nuzri@users.noreply.github.com>
2022-07-11 10:16:20 +0530

    Revert "declaring local variables and changing global variables to uppercase initial"
    This reverts commit f5a0694804a106e4bdf233c8a61819e227b15acf.

--- main.lua
+++ main.lua
@@ -2,40 +2,44 @@ require 'target'
 require 'menu-button'
 
 function love.load()
-    love.filesystem.setIdentity('aim-trainer')
+	love.filesystem.setIdentity('aim-trainer')
 	math.randomseed(os.time())
 
 	--cursor
+	---@diagnostic disable-next-line: missing-parameter
     local cursorImg = love.graphics.newImage('gfx/cursor.png')
     cursor = love.mouse.newCursor('gfx/cursor.png', cursorImg:getWidth() / 2, cursorImg:getHeight() / 2)
 
 	--images
+    ---@diagnostic disable-next-line: missing-parameter
     heart = love.graphics.newImage('gfx/heart.png')
 
 	--fonts
-    LargeFont = love.graphics.newFont('fonts/Poppins-Regular.ttf', 43)
-    MediumFont = love.graphics.newFont('fonts/Poppins-Regular.ttf', 30)
-    SmallFont = love.graphics.newFont('fonts/Poppins-Regular.ttf', 20)
+    largeFont = love.graphics.newFont('fonts/Poppins-Regular.ttf', 43)
+    mediumFont = love.graphics.newFont('fonts/Poppins-Regular.ttf', 30)
+    smallFont = love.graphics.newFont('fonts/Poppins-Regular.ttf', 20)
 
 	--audios
-    ScoreAudio = love.audio.newSource('sounds/score.mp3', 'static')
-    MissAudio = love.audio.newSource('sounds/miss.wav', 'static')
-    MisclickAudio = love .audio.newSource('sounds/misclick.mp3', 'static')
-    ButtonAudio = love.audio.newSource('sounds/button-press.wav', 'static')
+    scoreAudio = love.audio.newSource('sounds/score.mp3', 'static')
+    missAudio = love.audio.newSource('sounds/miss.wav', 'static')
+    misclickAudio = love .audio.newSource('sounds/misclick.mp3', 'static')
+    buttonAudio = love.audio.newSource('sounds/button-press.wav', 'static')
+
 
 	--initialising variables
 	w, h = love.graphics.getDimensions()
-	InitLineWidth = love.graphics.getLineWidth()
-	GameState = 'start'
-	Score = 0
-	Highscore = 0
-	Lives = 3
-	Miss = 0
-	Try = 0
+	initLineWidth = love.graphics.getLineWidth()
+	gameState = 'start'
+	score = 0
+	highscore = 0
+	lives = 3
+	misses = 0
+	try = 0
 	t = math.random(0.8, 1)
-	ButtonWidth = 270
-	TitleY = (h / 2) - 130
-	ScoreY = 0
+	titleContent = 'Aim Trainer'
+	buttonWidth = 270
+	titleY = (h / 2) - 130
+	scoreY = 0
 	targetIndex = 0
 	targetsEliminated = 0
 	totalPlayTime = 0
@@ -43,59 +47,61 @@ function love.load()
 	transparent = {1, 1, 1, 0}
 	orange = {1.00000000000, 0.34117647059, 0.13333333333}
 
+
 	--initialise menu buttons
 	startMenuButton =
 	newMenuButton({'start game', 'sound', 'quit'},
 	{function()
-		ButtonAudio:play()
+		buttonAudio:play()
 		targetIndex = 1
 		target:new(targetIndex)
 		initPlayTime = love.timer.getTime()
-		Try = Try + 1
-		GameState = 'play'
+		try = try + 1
+		gameState = 'play'
 	end,
     function ()
-		ButtonAudio:play()
-		prevGameState = GameState
+		buttonAudio:play()
+		prevGameState = gameState
 	end,
     function ()
 		love.event.quit()
 	end},
-	'center', (h / 2) - 55, ButtonWidth, 55, 26, 12, white, transparent, orange, orange)
+	'center', (h / 2) - 55, buttonWidth, 55, 26, 12, white, transparent, orange, orange)
 
-	GameOverMenuButton =
+	gameOverMenuButton =
 	newMenuButton({'play again', 'sound', 'quit'},
     {function ()
-		if Score > Highscore then
-			Highscore = Score
+		if score > highscore then
+			highscore = score
 		end
-		ButtonAudio:play()
-    	Score = 0
-		Lives = 3
+		buttonAudio:play()
+    	score = 0
+		lives = 3
 		initPlayTime = love.timer.getTime()
-		Miss = 0
-		Try = Try + 1
+		misses = 0
+		try = try + 1
 		targetsEliminated = 0
 		targetIndex = 1
 		target:new(targetIndex)
 		t = math.random(0.8, 1)
-		GameState = 'play'
+		gameState = 'play'
     end,
     function ()
-		ButtonAudio:play()
-		prevGameState = GameState
+		buttonAudio:play()
+		prevGameState = gameState
     end,
     function ()
     	love.event.quit()
     end},
-	'center', (h / 2) - 25, ButtonWidth, 55, 26, 12, white, transparent, orange, orange)
+	'center', (h / 2) - 25, buttonWidth, 55, 26, 12, white, transparent, orange, orange)
 
 	love.mouse.setCursor(cursor)
 	love.audio.setVolume(0.8)
 end
 
+
 function love.update(dt)
-	if GameState == 'play' then
+	if gameState == 'play' then
 		for i = 1, targetIndex do
 			if target[i].state == 'active' then
 				target:update(i, dt)
@@ -114,17 +120,80 @@ function love.update(dt)
 	end
 end
 
-local function drawHearts()
-	local heartWidth = heart:getWidth()
-    local heartPadding = 5
 
-	love.graphics.setColor(1, 1, 1, 1)
-	for i = 1, Lives do
-		love.graphics.draw(heart, w - (heartWidth + 10) * i + 10 - heartPadding, heartPadding)
+function love.draw()
+	love.graphics.setBackgroundColor(0.00000000000, 0.07450980392, 0.12156862745)
+
+	if gameState == "start" then
+		love.graphics.setLineWidth(initLineWidth)
+
+		love.graphics.setFont(largeFont)
+		local titleLen = largeFont:getWidth('Aim Trainer')
+		local x = (w / 2) - (titleLen / 2)
+
+		love.graphics.setColor(1, 1, 1)
+		love.graphics.print('Aim ', x, titleY)
+		love.graphics.setColor(1.00000000000, 0.34117647059, 0.13333333333)
+		love.graphics.print('Trainer', x + largeFont:getWidth('Aim '), titleY)
+
+		love.graphics.setFont(mediumFont)
+		startMenuButton:draw()
+
+	elseif gameState == "play" then
+		love.graphics.setLineWidth(initLineWidth)
+
+		target:draw()
+		drawHearts()
+
+		playTime = string.format('%.1f', (love.timer.getTime() - initPlayTime))
+
+		love.graphics.setFont(smallFont)
+		scoreboardHorizontal({"Hits: ", "Highscore: ", "Time: "}, {score, highscore, playTime.."s"}, 60, 5, 0, white, orange)
+
+	elseif gameState == "over" then
+		love.graphics.setLineWidth(initLineWidth)
+
+		target:draw()
+
+		love.graphics.setColor(0.00000000000, 0.07450980392, 0.12156862745, 0.9)
+		love.graphics.rectangle('fill', 0, 0, w, h)
+
+		love.graphics.setFont(mediumFont)
+		love.graphics.setColor(1.00000000000, 0.34117647059, 0.13333333333)
+		love.graphics.printf('Stats:', 0, 70, w, 'center')
+
+		love.graphics.setFont(love.graphics.newFont('fonts/Poppins-Regular.ttf', 22))
+		scoreboardVertical({'Hit Targets:', 'Accuracy:', 'Avg. Time:'}, {score.."/"..targetIndex, accuracy, avgTime}, 0, (w / 2) - ((buttonWidth - 37) / 2), 115, buttonWidth - 37, white, orange)
+
+		love.graphics.setFont(smallFont)
+		scoreboardHorizontal({"Hits: ", "Highscore: ", "Time: "}, {score, highscore, playTime.."s"}, 60, 5, 0, white, orange)
+
+		love.graphics.setFont(mediumFont)
+		gameOverMenuButton:draw()
 	end
 end
 
-local function scoreboardVertical(leftColumn, rightColumn, gap, x, y, width, leftTextColor, rightTextColor)
+
+function love.mousepressed(x, y, button)
+	if gameState == 'play' then
+		target:mousepressed(x, y, button)
+	end
+end
+
+
+function love.keypressed(key)
+	if key=="rctrl" then
+		debug.debug() -- game crashes
+	end
+end
+
+
+function distance(x1, y1, x2, y2)
+	return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2 )
+end
+
+
+function scoreboardVertical(leftColumn, rightColumn, gap, x, y, width, leftTextColor, rightTextColor)
 	local fontHeight = love.graphics.getFont():getHeight()
 	for i = 1, #leftColumn do
 		love.graphics.setColor(leftTextColor[1], leftTextColor[2], leftTextColor[3], leftTextColor[4])
@@ -135,7 +204,8 @@ local function scoreboardVertical(leftColumn, rightColumn, gap, x, y, width, lef
 	end
 end
 
-local function scoreboardHorizontal(left, right, gap, x, y, leftTextColor, rightTextColor)
+
+function scoreboardHorizontal(left, right, gap, x, y, leftTextColor, rightTextColor)
 	for i = 1, #left do
 		X = 0
 		for j = 1, i do
@@ -152,70 +222,13 @@ local function scoreboardHorizontal(left, right, gap, x, y, leftTextColor, right
 	end
 end
 
-function love.draw()
-	love.graphics.setBackgroundColor(0.00000000000, 0.07450980392, 0.12156862745)
 
-	if GameState == "start" then
-		love.graphics.setLineWidth(InitLineWidth)
+function drawHearts()
+	local heartWidth = heart:getWidth()
+    local heartPadding = 5
 
-		love.graphics.setFont(LargeFont)
-		local titleLen = LargeFont:getWidth('Aim Trainer')
-		local x = (w / 2) - (titleLen / 2)
-
-		love.graphics.setColor(1, 1, 1)
-		love.graphics.print('Aim ', x, TitleY)
-		love.graphics.setColor(1.00000000000, 0.34117647059, 0.13333333333)
-		love.graphics.print('Trainer', x + LargeFont:getWidth('Aim '), TitleY)
-
-		love.graphics.setFont(MediumFont)
-		startMenuButton:draw()
-
-	elseif GameState == "play" then
-		love.graphics.setLineWidth(InitLineWidth)
-
-		target:draw()
-		drawHearts()
-
-		playTime = string.format('%.1f', (love.timer.getTime() - initPlayTime))
-
-		love.graphics.setFont(SmallFont)
-		scoreboardHorizontal({"Hits: ", "Highscore: ", "Time: "}, {Score, Highscore, playTime.."s"}, 60, 5, 0, white, orange)
-
-	elseif GameState == "over" then
-		love.graphics.setLineWidth(InitLineWidth)
-
-		target:draw()
-
-		love.graphics.setColor(0.00000000000, 0.07450980392, 0.12156862745, 0.9)
-		love.graphics.rectangle('fill', 0, 0, w, h)
-
-		love.graphics.setFont(MediumFont)
-		love.graphics.setColor(1.00000000000, 0.34117647059, 0.13333333333)
-		love.graphics.printf('Stats:', 0, 70, w, 'center')
-
-		love.graphics.setFont(love.graphics.newFont('fonts/Poppins-Regular.ttf', 22))
-		scoreboardVertical({'Hit Targets:', 'Accuracy:', 'Avg. Time:'}, {Score.."/"..targetIndex, accuracy, avgTime}, 0, (w / 2) - ((ButtonWidth - 37) / 2), 115, ButtonWidth - 37, white, orange)
-
-		love.graphics.setFont(SmallFont)
-		scoreboardHorizontal({"Hits: ", "Highscore: ", "Time: "}, {Score, Highscore, playTime.."s"}, 60, 5, 0, white, orange)
-
-		love.graphics.setFont(MediumFont)
-		GameOverMenuButton:draw()
+	love.graphics.setColor(1, 1, 1, 1)
+	for i = 1, lives do
+		love.graphics.draw(heart, w - (heartWidth + 10) * i + 10 - heartPadding, heartPadding)
 	end
-end
-
-function love.mousepressed(x, y, button)
-	if GameState == 'play' then
-		target:mousepressed(x, y, button)
-	end
-end
-
-function love.keypressed(key)
-	if key=="rctrl" then
-		debug.debug() -- game crashes
-	end
-end
-
-function distance(x1, y1, x2, y2)
-	return math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2 )
 end
--- target.lua
+++ target.lua
@@ -9,7 +9,7 @@ function target:new(n)
 	self[n].rad = 1
 	self[n].state = 'active'
 	self[n].x = math.random(maxRad, w - maxRad)
-	self[n].y = math.random(maxRad + ScoreY + love.graphics.getFont():getHeight(), h - maxRad)
+	self[n].y = math.random(maxRad + scoreY + love.graphics.getFont():getHeight(), h - maxRad)
 end
 
 function target:draw()
@@ -34,6 +34,7 @@ function target:draw()
 	end
 end
 
+
 function target:update(m, dt)
 	if self[m].radInc == true then
         self[m].rad = self[m].rad + speed * dt
@@ -48,31 +49,32 @@ function target:update(m, dt)
 	if self[m].rad <= 0 then
 		self[m].state = 'eliminated'
 		targetsEliminated = targetsEliminated + 1
-		MissAudio:play()
-		Lives = Lives - 1
+		missAudio:play()
+		lives = lives - 1
 
-		if Lives ~= 0 then
+		if lives ~= 0 then
 			targetIndex = targetIndex + 1
 			target:new(targetIndex)
 		else
 			totalPlayTime = totalPlayTime + playTime
-			avgTime = Try == 1 and 'N|A' or string.format('%.1f', totalPlayTime / Try)..'s'
-			accuracy = ((Score == 0) and (Miss == 0)) and 'N|A' or string.format('%.1f', (Score / (Score + Miss)) * 100)..'%'
-			GameState = 'over'
+			avgTime = try == 1 and 'N|A' or string.format('%.1f', totalPlayTime / try)..'s'
+			accuracy = ((score == 0) and (misses == 0)) and 'N|A' or string.format('%.1f', (score / (score + misses)) * 100)..'%'
+			gameState = 'over'
 		end
 	end
 end
 
+
 function target:mousepressed(x, y, button)
 	targetDetected = false
 
-	if GameState == 'play' and button == 1 then
+	if gameState == 'play' and button == 1 then
 		for i = targetIndex, 1, -1 do
 			if self[i].state == 'active' and targetDetected == false then
 				local mouse_to_target = distance(x, y, self[i].x, self[i].y)
 				if mouse_to_target <= self[i].rad then
-					Score = Score + 1
-					ScoreAudio:play()
+					score = score + 1
+					scoreAudio:play()
 					targetDetected = true
 					targetIndex = targetIndex + 1
 					target:new(targetIndex)
@@ -81,8 +83,8 @@ function target:mousepressed(x, y, button)
 				end
 			end
 			if i == 1 and targetDetected == false then
-				MisclickAudio:play()
-				Miss = Miss + 1
+				misclickAudio:play()
+				misses = misses + 1
 			end
 		end
 	end
